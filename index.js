a12e78bab6bc412766a736c70f4c15100d0d39c7
Mathias JP <175939829+Jaegermeisty@users.noreply.github.com>
2025-04-30 13:30:29 +0200

    Create 256 square divs on the page using JavaScript

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const mainContainer = document.querySelector('.main-container')
 
+function createDivs() {
+  for (let i = 0; i < 256; i++) {
+  const square = document.createElement('div');
+  square.classList.add('square');
+  mainContainer.appendChild(square);
+  }
+}
+
+createDivs()
