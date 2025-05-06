3ec6e63ff4fce415d4085d545d7c143bdb5c118b
Mathias JP <175939829+Jaegermeisty@users.noreply.github.com>
2025-05-06 12:57:09 +0200

    Created eventlistener to button with prompt and then wiping the innerhtml of the main container

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const mainContainer = document.querySelector('.main-container')
+const button = document.querySelector('.btn');
 
 
-  for (let i = 0; i < 256; i++) {
+for (let i = 0; i < 256; i++) {
   const square = document.createElement('div');
   square.classList.add('square');
   mainContainer.appendChild(square);
+}
+
+const squares = document.querySelectorAll('.square');
+
+squares.forEach(square => {
+  square.addEventListener('mouseover', () => {
+  square.classList.add('green');
+})
+});
+
+button.addEventListener('click', () => {
+  newDimension = prompt('Select your new dimesions (max 100).')
+  if (newDimension < 0 || newDimension > 100 || isNaN(newDimension)) {
+    alert('Select a number between 0-99.');
+    return;
   }
-
-  const squares = document.querySelectorAll('.square');
-
-  squares.forEach(square => {
-    square.addEventListener('mouseover', () => {
-    square.classList.add('green');
-  })
-  });
-
-  
+  mainContainer.innerHTML = '';
+});
