4ea3b085ae13a8ba92d6ecd2bf93d080fe1bd2ec
Mathias JP <175939829+Jaegermeisty@users.noreply.github.com>
2025-05-06 11:36:39 +0200

    Removed the function surrounding the for loop. Not needed. At least for now

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const mainContainer = document.querySelector('.main-container')
 
-function createDivs() {
+
   for (let i = 0; i < 256; i++) {
   const square = document.createElement('div');
   square.classList.add('square');
   mainContainer.appendChild(square);
   }
-}
-
-createDivs()
